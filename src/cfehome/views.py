23e1a96e44476643318f7d6d8de8da95b899cc0d
unknown <mohamed.mustafa.soliman.31@gmail.com>
2024-12-01 13:26:20 +0200

    Hello Saas

---
+++ src/cfehome/views.py
@@ -0,0 +1,34 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from visits.models import PageVisit
+
+this_file_path = pathlib.Path(__file__).parent
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_page_visit_count": qs.count(),
+    }
+    path = request.path
+    print("path:", path)
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
+
+
+
+
+# def home_page_view(request, *args, **kwargs):
+#     print(this_file_path)
+#     html_ = ""
+#     html_file_path = this_file_path / "home.html"
+#     html_ = html_file_path.read_text()
+#     return HttpResponse(html_)
+
